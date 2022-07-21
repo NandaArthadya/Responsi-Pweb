ea5bb760fcfde8d6e447f2ed4f196487f7e32801
NandaArthadya <101624924+NandaArthadya@users.noreply.github.com>
2022-07-21 22:19:09 +0700

    Responsi Pweb

--- lihat.php
+++ lihat.php
@@ -1,20 +1,61 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Database</title>
+    <style type="text/css">
+        body{
+            margin: 0;
+            padding: 0;
+            position: relative;
+            font-family: Roboto;
+            background-image: url('jnne.jpg');
+            background-repeat: no-repeat;
+            background-position: center;
+            height: 100vh;
+            overflow: hidden;
+            font-size: 15px;
+        }
+        .center{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-55%, -55%);
+            width: 70vh;
+            background: white;
+            border-radius: 10px;
+        }
+        .center h1{
+            text-align: center;
+            padding: 0 0 10px 0;
 
-echo "<head><title>My Guest Book</title></head>";
-$fp = fopen("file.txt", "r");
+        }
+    </style>
+</head>
+<body>
+    <div class="center">
+        <h1>DATA BASE</h1>
+        <?php 
 
-echo "<table border='5'>";
-while ($isi = fgets($fp)) {
-    $pisah = explode('|', $isi);
-    echo "<tr><td>Tanggal: </td><td>: $pisah[0] </td></tr>";
-    echo "<tr><td>Nama: </td><td>: $pisah[1] </td></tr>";
-    echo "<tr><td>Kota Asal: </td><td>: $pisah[2] </td></tr>";
-    echo "<tr><td>Kota Tujuan: </td><td>: $pisah[3] </td></tr>";
-    echo "<tr><td>Jarak: </td><td>: $pisah[4] </td></tr>";
-    echo "<tr><td>Total Bayar: </td><td>: $pisah[5] </td></tr>";
-}
+            echo "<head><title>My Guest Book</title></head>";
+            $fp = fopen("file.txt", "r");
 
-echo "</table>";
-echo "<a href='index.php'> Mengisi Database </a>";
+            echo "<table border='0' align='center'>";
+            while ($isi = fgets($fp)) {
+            $pisah = explode('|', $isi);
+            echo "<tr><td>Tanggal: </td><td>: $pisah[0] </td></tr>";
+            echo "<tr><td>Nama: </td><td>: $pisah[1] </td></tr>";
+            echo "<tr><td>Kota Asal: </td><td>: $pisah[2] </td></tr>";
+            echo "<tr><td>Kota Tujuan: </td><td>: $pisah[3] </td></tr>";
+            echo "<tr><td>Jarak: </td><td>: $pisah[4] </td></tr>";
+            echo "<tr><td>Total Bayar: </td><td>: $pisah[5] </td></tr>";
+            }
 
-?>
+            echo "</table>";
+            echo "<a href='index.php'> Mengisi Database </a>";
+
+        ?>
+    </div>
+</body>
+</html>
--- proses.php
+++ proses.php
@@ -1,18 +1,41 @@
-<?php 
-$Nama = $_POST['inama'];
-$Jarak = $_POST['ijarak'];
-$Asal= $_POST['iasal'];
-$Tujuan= $_POST['itujuan'];
-$Total_Bayar=$_POST['ototal'];
-$tanggal= date("Y-m-d");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style type="text/css">
+        body{
+            font-family: roboto;
+            background-color: #ADD8E6 ;
+        }
+        .container{
+            position: relative;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+  <div class="container">
+        <h1 align="center">Terima Kasih Telah Menggunakan Jasa Kami</h1>
+      <?php 
+        $Nama = $_POST['inama'];
+        $Jarak = $_POST['ijarak'];
+        $Asal= $_POST['iasal'];
+        $Tujuan= $_POST['itujuan'];
+        $Total_Bayar=$_POST['ototal'];
+        $tanggal= date("Y-m-d");
 
-echo "<head><title>My Guest Book</title></head>";
-$fp = fopen("file.txt", "a+");
-fputs($fp, "$tanggal|$Nama|$Asal|$Tujuan|$Jarak|$Total_Bayar\n");
-fclose($fp);
+        echo "<head><title>My Guest Book</title></head>";
+        $fp = fopen("file.txt", "a+");
+        fputs($fp, "$tanggal|$Nama|$Asal|$Tujuan|$Jarak|$Total_Bayar\n");
+        fclose($fp);
 
-echo "Lihat Database<br>";
-echo "<a href='index.php'>Link mengisi data<br>";
-echo "<a href='lihat.php'>Lihat Database</a><br>";
+        echo "Lihat Database<br>";
+        echo "<a href='index.php'>Link mengisi data<br>";
+        echo "<a href='lihat.php'>Lihat Database</a><br>";
 
- ?>
+        ?>
+  </div>
+</body>
+</html>
